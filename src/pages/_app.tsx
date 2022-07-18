27b3b8d735cda72f0fa58b7586a156189addaf86
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-07-18 09:46:28 -0300

    ALterado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import { Header } from './components/Header'
 
+import { Provider as NextAuthProvider } from 'next-auth/react'
+
 import './styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/components/SignInButton/index.tsx
+++ src/pages/components/SignInButton/index.tsx
@@ -1,11 +1,15 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/react'
+
+
 import styles from './styles.module.scss'
 
-export function SignInButton() {
-    const isUserLoggedIn = false;
 
-    return isUserLoggedIn ? (
+export function SignInButton() {
+    const { data: session } = useSession()
+
+    return session ? (
         <button className={styles.siginInButton}
             type="button"
         >
@@ -16,6 +20,7 @@ export function SignInButton() {
     ) : (
         <button className={styles.siginInButton}
             type="button"
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
