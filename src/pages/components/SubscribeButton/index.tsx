7ab9ce1ec0ac407a90785b464ca63e8d3d7c4a97
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-07-26 23:23:37 -0300

    alterado

--- src/pages/components/SubscribeButton/index.tsx
+++ src/pages/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { Console } from 'console';
 import { signIn, useSession } from 'next-auth/react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -11,18 +12,19 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession();
 
+
     async function handleSubscribe() {
+        const stripe = require('stripe')('sk_test_51LGx5YFyhmVLaoPJtOnq7O8X5kBvYYLHqewNwPduJwuPe6oQgLxB63iOgTi7n2iizjz1Jm75fOvkNUdFIYf5i8qU00j2hdZFPi');
         if (!session) {
             signIn('github')
             return;
         }
         try {
-            console.log(session);
             const respose = await api.post('/subscribe')
 
             const { sessionId } = respose.data;
 
-            const stripe = await getStripeJs()
+
 
 
             stripe.redirectToCheckout({ sessionId })
